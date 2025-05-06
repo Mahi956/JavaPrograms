bae0e65a365fc1f61d39df5ee320eecd4a8a8b68
Mahi Sharma <itsmahi.sharma17@gmail.com>
2025-05-06 10:46:38 +0530

    Add files via upload

---
+++ Thiskeyword.java
@@ -0,0 +1,28 @@
+public class Thiskeyword {
+    // int age =15;
+    // void data(){
+    //     System.out.println(this);
+    // }
+    // public static void main(String[] args) {
+    //     Thiskeyword obj =new Thiskeyword();
+    //     obj.data();
+    //     System.out.println(obj.age);
+    // }
+    
+}
+
+class Student {
+    int age ;
+    Student(int age){
+        this.age=age;
+    }
+    
+    void print(){
+        System.out.println(age);
+    }
+
+    public static void main(String[] args) {
+        Student std= new Student (8);
+        std.print();
+    }
+}
